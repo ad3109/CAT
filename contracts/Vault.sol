3271e25bee29ba18a1569cb4240317f98a2b4e82
Reginald Dewil <reginald@hatamoto.be>
2022-10-26 19:11:18 +0200

    some placeholder addresses for tokens

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,10 +4,19 @@ pragma solidity ^0.8.17;
 /*  This is a generic vault where a set of loans are held
     Loans are  */
 
-import {Loan} from "./Loan.sol";
+import "./Commodity.sol";
+import "./Loan.sol";
 
 contract Vault {
     address public immutable i_token; //address of commodity token
     mapping<address, Loan> private immutable s_addressesToLoans; 
+
+    constructor(address commodityAddress){
+        i_token = commodityAddress;
+    }
+
+    function public borrow(uint256 toBorrowAmount) public{
+        //TODO: create a loan if no loan already exists for that address and this token
+    }
 
 }
