ec60620b277de1568408e20ffb7334caf5439fdc
Reginald Dewil <reginald@hatamoto.be>
2022-10-27 12:29:48 +0200

    added CAT token,  is minted by the Vault contract - Vault contracts will be  deployed in deploy javascript

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,18 +4,32 @@ pragma solidity ^0.8.17;
 /*  This is a generic vault where a set of loans are held
     Loans are  */
 
+import "./CAT.sol";
 import "./Loan.sol";
 
 contract Vault {
-    address public immutable i_token; //address of commodity token
-    mapping(address => Loan) private s_addressesToLoans;
+    CAT private immutable i_token;
+    mapping(address => Loan) private s_addressesToLoans; //the list of outstanding loans
 
-    constructor(address commodityAddress) {
-        //TODO: add allowed collateral as input paramater for Vault
-        i_token = commodityAddress;
+    constructor(string memory name, string memory symbol) {
+        i_token = new CAT(0, name, symbol);
     }
 
     function borrow(uint256 toBorrowAmount) public {
-        //TODO: create a loan if no loan already exists for that address and this token
+        /**1. check if this user is allowed to borrow: i.e. has remaining free collateral
+         * 2. mint the tokens & send to user
+         * 3. update Loan object
+         * 4. add Loan object to s_addressesToLoans
+         */
+
+        //TODO: 1. check if user is allowed to borrw
+        //2. mint the tokens and send to user
+        i_token._mintCAT(msg.sender, toBorrowAmount);
+
+        //3. update Loan object
+    }
+
+    function getToken() public view returns (CAT) {
+        return i_token;
     }
 }
