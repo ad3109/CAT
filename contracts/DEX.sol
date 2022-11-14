a8026ef584a9fffafe642b5da6404f760f90d7ef
bchain <109272510+bchain3@users.noreply.github.com>
2022-11-14 19:08:25 +0530

    Update DEX.sol
    fixed a few problems

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-// Deposit and withdraw liquidity not working as of now
-
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -27,8 +25,8 @@ contract DEX is Ownable {
     // mapping to link token to its available liquidity
     mapping(address => uint256) public tokenAddressToLiquidity;
 
-    // mapping to link liquidity provider address to token wise total liquidity provided
-    mapping(address => mapping(address => uint256)) liquidityProviderToLiquidityProvided;
+    // mapping to link liquidity provider address to token wise total liquidity provided i.e. number of tokens provided
+    mapping(address => mapping(address => uint256)) public liquidityProviderToLiquidityProvided;
 
     // mapping to track available liquidity pools and th
     mapping (address => mapping(address => lpAmounts)) public liquidityPoolToAvailableLiquidity;
@@ -61,7 +59,7 @@ contract DEX is Ownable {
         uint256 _tokenPriceUSD) 
     public payable returns(uint256) {
         require(_token != address(0), "Enter valid address!");
-        require(msg.value >= 0, "Enter valid ETH amount!");
+        require(msg.value > 0, "Enter valid ETH amount!");
         ERC20 token = ERC20(_token);
        
         // if pool doesnt exist
@@ -72,9 +70,10 @@ contract DEX is Ownable {
             require(token.balanceOf(msg.sender) >= deltaToken);
             require(token.transferFrom(msg.sender, address(this), deltaToken), "Token transfer failed!");
 
-            tokenAddressToLiquidity[WETH] = msg.value;
+            tokenAddressToLiquidity[WETH] = tokenAddressToLiquidity[WETH].add(msg.value);
             tokenAddressToLiquidity[_token] = deltaToken;
 
+            // mapping to change later
             liquidityProviderToLiquidityProvided[msg.sender][_token] = liquidityProviderToLiquidityProvided[msg.sender][_token].add(deltaToken);
             liquidityProviderToLiquidityProvided[msg.sender][WETH] = liquidityProviderToLiquidityProvided[msg.sender][WETH].add(msg.value);
 
@@ -87,14 +86,25 @@ contract DEX is Ownable {
         // if pool already exists
         else {
             lpAmounts memory amounts = liquidityPoolToAvailableLiquidity[WETH][_token];
-            //uint256 ethReserves = address(this).balance.sub(msg.value);
-            uint256 ethReserves = amounts.tokenA;
-            uint256 tokenReserves = token.balanceOf(address(this));
-            uint256 poolRatio = (ethReserves.mul(10 ** 18)).div(tokenReserves);
 
-            //uint256 deltaToken = (((ethReserves + msg.value).mul(poolRatio)).div(10 ** 18)).sub(tokenReserves);
-            uint256 termOne = (((ethReserves + msg.value) * (poolRatio)) / (10 ** 18));
-            uint256 deltaToken = termOne > tokenReserves ? termOne - tokenReserves : tokenReserves - termOne;
+            uint256 ethReserves = amounts.tokenA;
+            uint256 tokenReserves = amounts.tokenB;
+            
+            uint256 tokenPriceInEth = ((tokenReserves * (10 ** 18)) / ethReserves);
+            uint256 deltaToken = msg.value.mul(tokenPriceInEth).div(10 ** 18);
+
+            require(token.balanceOf(msg.sender) >= deltaToken, "You dont have enough token balance!");
+            require(token.transferFrom(msg.sender, address(this), deltaToken), "Token transfer failed!");
+
+            tokenAddressToLiquidity[WETH] = tokenAddressToLiquidity[WETH].add(msg.value);
+            tokenAddressToLiquidity[_token] = tokenAddressToLiquidity[WETH].add(deltaToken);
+
+            liquidityProviderToLiquidityProvided[msg.sender][_token] = liquidityProviderToLiquidityProvided[msg.sender][_token].add(deltaToken);
+            liquidityProviderToLiquidityProvided[msg.sender][WETH] = liquidityProviderToLiquidityProvided[msg.sender][WETH].add(msg.value);
+
+            amounts.tokenA = amounts.tokenA.add(msg.value);
+            amounts.tokenB = amounts.tokenB.add(deltaToken);
+            liquidityPoolToAvailableLiquidity[WETH][_token] = amounts;
 
             emit liquidityAdded(WETH, _token, amounts.tokenA, amounts.tokenB);
             return deltaToken;          
@@ -115,34 +125,29 @@ contract DEX is Ownable {
         lpAmounts memory poolAmounts = liquidityPoolToAvailableLiquidity[WETH][_token];
         require((poolAmounts.tokenA != 0 && poolAmounts.tokenB != 0), "No liquidity for this pool");
 
+        uint256 ethAmountInPool = poolAmounts.tokenA;
+        uint256 tokenAmountInPool = poolAmounts.tokenB;
+
         require(liquidityProviderToLiquidityProvided[msg.sender][WETH] >= _ethAmount, "You have not provided this much liquidity for eth!");
         require(liquidityProviderToLiquidityProvided[msg.sender][_token] >= _tokenAmount, "You have not provided this much liquidity for token!");
 
-        liquidityPoolToAvailableLiquidity[msg.sender][WETH] = liquidityPoolToAvailableLiquidity[msg.sender][WETH].sub(_ethAmount);
-        liquidityPoolToAvailableLiquidity[msg.sender][_token] = liquidityPoolToAvailableLiquidity[msg.sender][_token].sub(_tokenAmount);
-
-        lpAmounts memory amounts = liquidityPoolToAvailableLiquidity[WETH][_token];
-        amounts.tokenA = amounts.tokenA.sub(_ethAmount);
-        amounts.tokenB = amounts.tokenB.sub(_tokenAmount);
-        liquidityPoolToAvailableLiquidity[WETH][_token] = amounts;
-
+        // case 1: number of tokens in pool > number of tokens user is asking for
+        liquidityProviderToLiquidityProvided[msg.sender][WETH] = liquidityProviderToLiquidityProvided[msg.sender][WETH].sub(_ethAmount);
+        liquidityProviderToLiquidityProvided[msg.sender][_token] = liquidityProviderToLiquidityProvided[msg.sender][_token].sub(_tokenAmount);
+        poolAmounts.tokenA = poolAmounts.tokenA.sub(_ethAmount);
+        poolAmounts.tokenB = poolAmounts.tokenB.sub(_tokenAmount);
+        liquidityPoolToAvailableLiquidity[WETH][_token] = poolAmounts;
         tokenAddressToLiquidity[WETH] = tokenAddressToLiquidity[WETH].sub(_ethAmount);
-        tokenAddressToLiquidity[_token] = tokenAddressToLiquidity[_token].sub(_tokenAmount;
-
-        msg.sender.transfer(_ethAmount);
+        tokenAddressToLiquidity[_token] = tokenAddressToLiquidity[_token].sub(_tokenAmount);
+        payable(msg.sender).transfer(_ethAmount);
         require(token.transfer(msg.sender, _tokenAmount));
-
-        emit liquidityRemoved(WETH, _token, amounts.tokenA, amounts.tokenB);
-        return (_ethAmount, _tokenAmount)
+        emit liquidityRemoved(WETH, _token, poolAmounts.tokenA, poolAmounts.tokenB);
+        return (_ethAmount, _tokenAmount);
     }
 
 
     // function to calculate price to swap based on AMM
-    function getSwapPrice(
-        uint256 xInput, 
-        uint256 xReserves, 
-        uint256 yReserves) 
-    public pure returns(uint256 yOutput) {
+    function getSwapPrice(uint256 xInput, uint256 xReserves, uint256 yReserves) public pure returns(uint256 yOutput) {
         uint256 xInputWithFee = xInput.mul(997);
         uint256 numerator = xInputWithFee.mul(yReserves);
         uint256 denominator = (xReserves.mul(1000)).add(xInputWithFee);
@@ -164,9 +169,11 @@ contract DEX is Ownable {
         uint256 tokenReserves = amounts.tokenB;
         uint256 tokensBought = getSwapPrice(msg.value, ethReserv, tokenReserves);
 
+        require((ethReserv >= msg.value) && (tokenReserves >= tokensBought), "Not enough liquidity!");
+
+        tokenAddressToLiquidity[WETH] = tokenAddressToLiquidity[WETH].add(msg.value);
         tokenAddressToLiquidity[_token] = tokenAddressToLiquidity[_token].sub(tokensBought);
 
-        lpAmounts memory amounts = liquidityPoolToAvailableLiquidity[WETH][_token];
         amounts.tokenA = amounts.tokenA.add(msg.value);
         amounts.tokenB = amounts.tokenB.sub(tokensBought);
         liquidityPoolToAvailableLiquidity[WETH][_token] = amounts;
@@ -182,13 +189,15 @@ contract DEX is Ownable {
 
         lpAmounts memory amounts = liquidityPoolToAvailableLiquidity[WETH][_token];
         
-        uint256 ethReserv = amounts.tokenA;
+        uint256 ethReserves = amounts.tokenA;
         uint256 tokenReserves = amounts.tokenB;
+        uint256 ethBought = getSwapPrice(_tokenAmount, tokenReserves, ethReserves);
 
-        uint256 ethBought = getSwapPrice(_tokenAmount, tokenReserves, ethReserv);
+        require((ethReserves >= ethBought) && (tokenReserves >= _tokenAmount), "Not enough liquidity!");
+
+
         payable(msg.sender).transfer(ethBought);
 
-        lpAmounts memory amounts = liquidityPoolToAvailableLiquidity[WETH][_token];
         amounts.tokenA = amounts.tokenA.sub(ethBought);
         amounts.tokenB = amounts.tokenB.add(_tokenAmount);
         liquidityPoolToAvailableLiquidity[WETH][_token] = amounts;
